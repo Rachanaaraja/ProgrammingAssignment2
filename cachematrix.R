c0cdc1f3c2bd58e7108e59c381e453611e5cf1f6
Rachanaaraja <65718185+Rachanaaraja@users.noreply.github.com>
2020-05-21 19:35:37 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##makeCacheMatrix fuction creates a special matrix object that can cache its inverse
+##cacheSolve function computes the inverse of the matrix returned by makeCacheMatrix
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function has a list of functions to set the value of matrix,get the matrix value,set the inverse value of the matrix and to get the inverse value of the matrix
+makeCacheMatrix <- function(x = matrix()) #input matrix as argument
+{
+  inver<-NULL   #initial inverse value to null
+  set<-function(y)   #set function to set input matrix value
+  {
+    x<<-y
+    #print(x)
+  }
+  
+  get<-function() x  #get function to get matrix value 
+  setinverse<-function(inverse) inver <<-inverse   #setinverse function to set inverse matrix value
+  getinverse<-function() inver                     #getinverse function to get inverse matrix value
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)  #list of functions
+  
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) 
+{
+  matdata<-x$get()                       #matdata stores the matrix data from makeCachematrix function
+  inver<-solve(matdata,...)              #inver stores the inverse matrix computed by solve function
+  x$setinverse(inver)                   
+  inver<-x$getinverse()
+  if(!is.null(inver))
+  {
+    print("Inverse of the given matrix")
+    return(inver)                          #returns the inverse matrix value
+  }
 }
+
+mat<-matrix(c(1,2,3,0,1,4,5,6,0),3,3)      #matrix input
+print("The input matrix")
+print(mat)
+cacheout<-makeCacheMatrix(mat)             #creates special matrix object that cache its inverse
+inversionans<-cacheSolve(cacheout)         #computes the inverse of input matrix and returns the value to inversionans
+print(inversionans)                        #inverse matrix as output returned from cacheSOlve function
